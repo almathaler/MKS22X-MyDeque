1c06a6826bfe458175d1358cfbf21a81785be03e
almathaler <athaler00@stuy.edu>
2019-03-25 20:02:33 -0400

    done

--- MyDeque.java
+++ MyDeque.java
@@ -1,47 +1,26 @@
 import java.util.NoSuchElementException;
 import java.util.Arrays;
-//work on resize and also removing
-//resize is faulty check terminal
 public class MyDeque<E>{
   private E[] data;
   private int size = 0;
   private  int start = -1;
   private int end = -1;
   @SuppressWarnings("unchecked")
-  public static void main(String[] args){
-    MyDeque<Integer> test = new MyDeque();
-    for (int i = 0; i<10; i++){
-      test.addLast(i);
-    }
-    System.out.println("deque: " + test);
-    System.out.println("end is at: " + test.end);
-    System.out.println("start is at: " + test.start);
-    System.out.println("size is: " + test.size);
-    test.addLast(10);
-    System.out.println("added 10: " + test);
-    System.out.println("what the array looks like: " + Arrays.toString(test.data));
-  }
   public MyDeque(){
     data = (E[])new Object[10];
-    //size = 0;
   }
   public MyDeque(int initialCapacity){
     data = (E[])new Object[initialCapacity];
-    //size = 0;
   }
   public int size(){
     return size;
   }
   public String toString(){
-    //System.out.println("start: " + start);
-    //System.out.println("end: " + end);
-    //System.out.println("size: " + size);
     String toReturn = "{";
     if (size == 0){
       return "{}";
     }
     if (start == end){
-      //System.out.println("start == end");
       toReturn += data[0];
     }else if(start < end){
       for (int i = start; i<= end; i++){
@@ -60,35 +39,27 @@ public class MyDeque<E>{
   }
   //add to front, so push everything back one.
   public void addFirst(E element){
-    //System.out.println("Adding " + element);
     if (element == null){
       throw new NullPointerException("no null values allowed");
     }
     if (size == 0){
-      //System.out.println("in first block");
       data[0] = element;
       start = 0;
       end = 0;
       size++;
-      //(start == end && size == 1) || ((start == end + 1 || start == end - 1 || for below
     } else if (start == 0 && size < data.length){
-      //System.out.println("in second block");
       start = data.length - 1;
       data[start] = element;
       size++;
     } else if (start > (end + 1)){
-      //System.out.println("in third block");
       start--;
       data[start] = element;
       size++;
     } else if (start > 0 && start < end){
-      //System.out.println("in fourth block");
       start--;
       data[start] = element;
       size++;
     } else if ((start == end + 1 || start == end - 1) && size == data.length){
-      //System.out.println("in fifth block");
-      //System.out.println("resizing");
       resize(); //problem here -- went to this even when there is space to move back.
       start = 0;
       end = size-1;
@@ -115,37 +86,29 @@ public class MyDeque<E>{
     }
     data = toAssign;
   }
-  //REASSIGN FOR LOOP VARIABLES
   public void addLast(E element){
-    //System.out.println("Adding Last " + element);
     if (element == null){
       throw new NullPointerException("no null vals");
     }
     //&& start == -1 && end == -1
     if (size == 0){
-    //System.out.println("in first block");
       data[0] = element;
       start = 0;
       end = 0;
       size++;
     }else if (start == end && size == 1){
-      //System.out.println("in second block");
       end++;
       data[end] = element;
       size++;
     }else if(end < (start - 1)){
-      //System.out.println("in third block");
       end++;
       data[end] = element;
       size++;
     }else if (end > start && end < (data.length-1)){
-      //System.out.println("in fourth block");
       end++;
       data[end] = element;
       size++;
     }else if (((end == start + 1 || end == start - 1) && size == data.length) || size == data.length){ //resize
-      //System.out.println("in fifth block");
-      //System.out.println("resizing");
       resize();
       start = 0;
       end = size-1;
@@ -159,7 +122,6 @@ public class MyDeque<E>{
     }
     toReturn = data[start];
     if (start < (data.length-1)){ //if there is space to move start up 1
-      //toReturn = data[start];
       data[start] = null;
       start++;
       size--;
