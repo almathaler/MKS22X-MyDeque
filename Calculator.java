9bbf3241ac3d5dcf76dfb73173abaf30bfc13389
almathaler <athaler00@stuy.edu>
2019-03-26 10:51:31 -0400

    now only one deque

--- Calculator.java
+++ Calculator.java
@@ -1,74 +1,77 @@
 
 public class Calculator{
     public static void main(String[] args){
-      String s = "11 3 - 4 + 2.5 *";
-      //eval(s);
-      System.out.println("11 3 - 4 + 2.5 * + is:  " + eval(s));
+      String s = "1 2 3 4 5 + * - -";
+      System.out.println("1 2 3 4 5 + * - -  is:  " + eval(s));
     }
     /*Evaluate a postfix expression stored in s.
      *Assume valid postfix notation, separated by spaces.
      */
     public static double eval(String s){
-      MyDeque<String> queue = new MyDeque<String>(s.length());
+      MyDeque<String> queue = new MyDeque<String>(s.length()); //create a new queue to hold the tokens
+      //write the string into the queue
       String[] postfix = s.split(" ");
-      for (String st : postfix){
-        queue.addLast(st);
-      }
-      //System.out.println("the queue: " + queue.toString());
-      int size = queue.size();
-      //int size = postfix.length;
-      MyDeque<Double> stack = new MyDeque<Double>(size); //for the numbers
-      boolean working = true;
-      //int i = 0
-      while (working){
-        //System.out.println("getFirst: " + queue.getFirst());
-        String addS = queue.removeFirst();
-        //System.out.println("addS: " + addS + " at index: " + i);
+      //for (String st : postfix){
+      //  queue.addLast(st);
+      //}
+      //create a stack that will hold the numbers ( say u have {1 2 3 4 - + *}, stack will store 1 2 3 4 and
+      //then when u hit an opperator, will use 3 and 4)
+      //int size = queue.size();
+      MyDeque<Double> stack = new MyDeque<Double>(postfix.length); //for the numbers
+      //boolean working = true;
+      //stops working when queue size is 0 / you have gone through all the elements
+      for (int i = 0; i<postfix.length; i++){
+        //String addS = queue.removeFirst();
+        //see if the element is a number
+        String addS = postfix[i];
         try{
+          //if yes then add it to the stack
           Double toAdd = Double.parseDouble(addS);
           stack.addLast(toAdd);
         } catch (NumberFormatException e){
-          queue.addFirst(addS);
-          //System.out.println("addS wasn't a number, added it back to queue");
+          //else keep it in the queue
+          //queue.addFirst(addS);
+          //don't do anything... keep that value in the array
         }
-        //System.out.println("queue now: " + queue.toString() + " stack: " + stack.toString());
-        //REMEMBER EVEN THO UR GETTING FROM LAST TO FRONT, U DO THE
-        //OPERATIONS FROM FRONT TO BACK! SO THAT'S WHY IT IS
-      //B TO A
-        if (queue.getFirst().equals("+") && stack.size() >= 2){
+        //here are the operations
+        //if you've hit an opperator and there are 2 or more #s in the stack
+        //if (queue.getFirst().equals("+") && stack.size() >= 2){
+        if (postfix[i].equals("+") && stack.size() >= 2){
           Double a = stack.removeLast();
           Double b = stack.removeLast();
+          //the order is b then a, since although we are picking up from the top of the stack we still need to
+          //order apperation as leftmost number (operation) right digit
           Double toAdd = b+a;
-          queue.removeFirst(); // the opperator
+          //queue.removeFirst(); // the opperator -- don't have to remove anything cuz index goes further
           stack.addLast(toAdd);
-        }else if (queue.getFirst().equals("-") && stack.size() >= 2){
+        }else if (postfix[i].equals("-") && stack.size() >= 2){
           Double a = stack.removeLast();
           Double b = stack.removeLast();
           Double toAdd = b-a;
-          queue.removeFirst(); // the opperator
+          //queue.removeFirst(); // the opperator
           stack.addLast(toAdd);
-        }else if (queue.getFirst().equals("*") && stack.size() >= 2){
+        }else if (postfix[i].equals("*") && stack.size() >= 2){
           Double a = stack.removeLast();
           Double b = stack.removeLast();
           Double toAdd = b*a;
-          queue.removeFirst(); // the opperator
+          //queue.removeFirst(); // the opperator
           stack.addLast(toAdd);
-        }else if (queue.getFirst().equals("/") && stack.size() >= 2){
+        }else if (postfix[i].equals("/") && stack.size() >= 2){
           Double a = stack.removeLast();
           Double b = stack.removeLast();
           Double toAdd = b/a;
-          queue.removeFirst(); // the opperator
+          //queue.removeFirst(); // the opperator
           stack.addLast(toAdd);
-        }else if (queue.getFirst().equals("%") && stack.size() >= 2){
+        }else if (postfix[i].equals("%") && stack.size() >= 2){
           Double a = stack.removeLast();
           Double b = stack.removeLast();
           Double toAdd = b%a;
-          queue.removeFirst(); // the opperator
+          //queue.removeFirst(); // the opperator
           stack.addLast(toAdd);
         }
-        if (queue.size() == 0){
-          working = false;
-        }
+        //if (queue.size() == 0){
+        //  working = false;
+        //}
       }
       return stack.getLast();
     }
